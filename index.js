c39ca9ca9f65b6d7f7f71590a03614b8c279a28b
Damian V.N <dvnegrete@gmail.com>
2024-04-19 17:08:26 -0600

    PPrimeros ajustes para pruebas de Captura de cursos mediante interfaz

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 const functions = require('@google-cloud/functions-framework');
-const { getSpreedSheet } = require('./spreedSheet');
+const { getSpreedSheet, postSpreedSheet } = require('./spreedSheet');
+const coursesModel = require('./coursesModel');
 
 functions.http('C196', async (req, res) => {
-    const allowedOrigins = ['https://cecati196.edu.mx', 'http://localhost:4200', 'https://localhost:4200'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  const allowedOrigins = ['https://cecati196.edu.mx', 'http://localhost:4200', 'https://localhost:4200'];
+  const origin = req.headers.origin;
+  console.log(origin)
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  switch (req.method) {
+    case 'GET':
+      const rows = await getSpreedSheet();
+      const data = coursesModel(rows);
+      const infoResult = {data};
+      console.log("GET, antes del RES.json()")
+      res.json(infoResult);
+      break;
+    case 'POST':
+      await postSpreedSheet(req.body);
+      const result = await lastRegistration();
+      res.json({message: "Se añadio el Curso " + result });
+      break;
+    case 'DELETE':
+      console.log("voy a borrar este curso:", req.body)
+      res.json({message:"Curso Borrado"});
+      break;
+    case 'PATH':
+      console.log("Actualiza esta parte:", req.body)
+      res.json({message:"Curso Actualizado.", });
+      break;    
+    default:
+      console.log("Error en la petición")
+      res.json({message:"Error en la petición", });
+      break;
+  }
+});
+
+async function lastRegistration() {
+  const rows = await getSpreedSheet();
+  const countRows = rows.length;
+  const lastInscription = rows[countRows-1].get('curso');
+  return lastInscription;
+}
+
+functions.http('token', async (req, res) => {
+  //crear y probar para generar token desde cloud functions
+  const allowedOrigins = ['https://cecati196.edu.mx', 'http://localhost:4200', 'https://localhost:4200'];
+  const origin = req.headers.origin;
+  console.log(origin)
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
-    const infoResult = await getSpreedSheet();
-    res.json(infoResult);
-  });
+});
--- spreedSheet.js
+++ spreedSheet.js
@@ -1,7 +1,7 @@
 const { JWT } = require ('google-auth-library');
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
-const coursesModel = require('./coursesModel');;
+const coursesModel = require('./coursesModel');
 const { client_email, private_key, idsheet196Courses} = require('./config');
 
 async function conexionGoogleSheet() {
@@ -15,22 +15,52 @@ async function conexionGoogleSheet() {
     const doc = new GoogleSpreadsheet(idsheet196Courses, serviceAccountAuth);
     await doc.loadInfo();
     //Solo se esta usando la primera hoja del libro
-    const sheet = doc.sheetsByIndex[0];
+    //const sheet = doc.sheetsByIndex[0];
+
+    //Para ejecución de pruebas en BD-C196
+    const sheet = doc.sheetsByIndex[1];
     return sheet;
 }
 
 async function getSpreedSheet(){
     try {
         const sheet = await conexionGoogleSheet();
-        const rows = await sheet.getRows();
-        const data = coursesModel(rows);
-        const infoResult = {...data};
-        return infoResult;
+        const rows = await sheet.getRows();        
+        return rows;
     } catch (error) {
         console.log("Error en getSpreedSheat", error)
     }
 }
 
+async function postSpreedSheet(body) {
+    try {
+        const sheet = await conexionGoogleSheet();
+        const obj = {
+            curso: body.courseName,
+            especialidad: body.specialty,
+            contenido_tematico: body.thematicContent,
+            objetivo: body.objective,
+            hora_inicio: body.startTime,
+            hora_fin: body.endTime,
+            fecha_inicio: body.startDate,
+            fecha_termino: body.endDate,
+            dias_de_clases: body.daysOfClasses,
+            costo: body.cost,
+            profesor: body.professor,
+            horas: body.hours,
+            tipo_de_curso: body.courseType,
+            modalidad_curso: body.courseModality,
+            frase_busqueda: body.searchPhrase,
+            observaciones: body.observations,
+        }
+        await sheet.addRow(obj);
+        return sheet;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     getSpreedSheet: getSpreedSheet,
+    postSpreedSheet: postSpreedSheet,
 }
